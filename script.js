41d564ccbc33b873cf4873a0b6e6b7f89a0f2152
vlad <vladozavr99@gmail.com>
2019-04-23 16:44:27 +0300

    TRUE

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-function send_email(choise_val){
-    const result = choise_val
+function send_email(choise_key, choise_val){
+    const result = "Selected button: " + choise_key + " (" + choise_val + ")"
 
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "andrew0999@hotmail.com",
-        Password : "5b0d21de-a0e8-4a80-bdaa-660b8c5b075f",
-        To : 'vlp@25k.ru',
+        Host : "smtp25.elasticemail.com",
+        Username : "vladozavr99@gmail.com",
+        Password : "6863bbaf-1d9a-43c6-9825-a9cac6cb5b03",
+        To : 'vladozavr99@gmail.com',
         From : "test@gmail.com",
         Subject : "She answered!",
         Body : result
     }).then(
-       message => console.log(message)
+        message => console.log(message),
+        $("#result").removeClass("disabled"),
+        $("#action").addClass("disabled")
     );
 }
 
+$(() => {
+   $("#password_button").click( function () {
+       if ($("#key").val().toLowerCase() === "сдрбогиня"){
+           $("#password").addClass("disabled")
+           $("#action").removeClass("disabled")
+       }
+   })
 
-   $(() => {
-   
-
-    $("#walk_button").click(function () { send_email("что-то посмотреть") })
+    $("#walk_button").click(function () { send_email("1", "что-то посмотреть") })
+    $("#eat_button").click(function () { send_email("2", "что-то покушать") })
+    $("#game_button").click(function () { send_email("3", "чем-то заняться") })
 
 
 });
-
 
     
 
